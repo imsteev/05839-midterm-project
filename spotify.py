71c4c2c5f4780697dea9ff7dd5fcc63ab8f1b851
Stephen Chung <spchung@andrew.cmu.edu>
2018-04-01 19:08:00 -0400

    first commit

---
+++ spotify.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+
+# dataset from https://www.kaggle.com/edumucelli/spotifys-worldwide-daily-song-ranking/data
+def create_region_csv(region):
+    with open("./data.csv") as csvfile:
+        df = pd.read_csv(csvfile)
+        g = df.groupby('Region')
+        region_df = g.get_group(region)
+        filename = "./%s_data.csv" % region
+        with open(filename, "w") as csvtosave:
+            region_df.to_csv(csvtosave, index=False, columns=['Position', 'Track Name', 'Artist', 'Streams', 'Date'])
+
+# create_region_csv('us')
+
+# Other data to extract: jumps above and below current spot. 
+# http://bl.ocks.org/wpoely86/e285b8e4c7b84710e463
+with open ("./us_data.csv") as csvfile:
+    df = pd.read_csv(csvfile)
+
+song_dfs = []
+
+for track_name in df['Track Name'].unique():
+    song_df = df[df['Track Name'] == track_name]
+    df.loc[df['Track Name'] == track_name, 'Position Diff'] = song_df['Position'] - song_df.shift()['Position']
+
+print(len(df))
