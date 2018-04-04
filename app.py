4dd1c7a9dbfd222b11a01db03a887db41d5198e2
Stephen Chung <spchung@andrew.cmu.edu>
2018-04-04 01:26:56 -0400

    display plots on website

--- app.py
+++ app.py
@@ -1,17 +1,47 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request, redirect, Markup
 from datetime import datetime
-app = Flask(__name__)
+from spotify import Spotify
+
+app = Flask(__name__, static_url_path='/static')
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+S = Spotify()
+selected_songs = []
 
 @app.route('/')
 def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+    position_scatter = False
+    position_heatmap = False
+    position_boxplots = False
+    if len(selected_songs) > 0:
+        position_scatter = Markup(S.create_scatter("Position", selected_songs, Spotify.POSITION_SCATTER_LAYOUT))
+        position_heatmap = Markup(S.create_heatmap("Position", selected_songs, Spotify.POSITION_HEATMAP_LAYOUT))
+        position_boxplots = Markup(S.create_boxplots("Position", selected_songs))
+    return render_template("index.html", 
+                            all_songs=S.all_songs, 
+                            artists=S.artists,
+                            selected_songs=selected_songs, 
+                            scatter=position_scatter, 
+                            heatmap=position_heatmap,
+                            boxplot=position_boxplots)
 
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
+@app.route('/loaddata')
+def load_data():
+    S.load_data()
+    return redirect(url_for('homepage'))
 
-    <img src="http://loremflickr.com/600/400">
-    """.format(time=the_time)
+@app.route('/selectsongs', methods=['POST'])
+def select_songs():
+    del selected_songs[:]
+    for key in request.form:
+        song = request.form[key]
+        trackname = song.split("---")[0]
+        artist = song.split("---")[1]
+        selected_songs.append(tuple((trackname, artist)))
+    return redirect(url_for('homepage'))
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html")
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+    app.run(use_reloader=True)  
--- spotify.py
+++ spotify.py
@@ -1,21 +1,221 @@
-import numpy as np
 import pandas as pd
+import numpy as np
+import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
 
-# dataset from https://www.kaggle.com/edumucelli/spotifys-worldwide-daily-song-ranking/data
-def create_region_csv(region):
-    with open("./data.csv") as csvfile:
-        df = pd.read_csv(csvfile)
-        g = df.groupby('Region')
-        region_df = g.get_group(region)
-        filename = "./%s_data.csv" % region
-        with open(filename, "w") as csvtosave:
-            region_df.to_csv(csvtosave, index=False, columns=['Position', 'Track Name', 'Artist', 'Streams', 'Date'])
+class Spotify(object):
+    POSITION_SCATTER_LAYOUT = {
+        'title': 'Song Position Scatterplot',
+        'yaxis': {
+            'autotick': False,
+            'ticks': '',
+            'tick0': 0,
+            'dtick': 20,
+            'ticklen': 200,
+            'tickcolor': '#000'
+        },
+        'shapes' : [
+            # highlight the region associated with top 10 songs
+            {
+                'type': 'rect',
+                'x0': '2017-01-01',
+                'y0': 0,
+                'x1': '2018-01-07',
+                'y1': 100,
+                'fillcolor': 'yellow',
+                'opacity': 0.2,
+                'line': {
+                    'width': 0
+                }  
+            },
+            {
+                'type': 'rect',
+                'x0': '2017-01-01',
+                'y0': 100,
+                'x1': '2018-01-07',
+                'y1': 200,
+                'fillcolor': 'red',
+                'opacity': 0.2,
+                'line': {
+                    'width': 0
+                }  
+            }
+        ]
+    }
 
-# create_region_csv('us')
+    POSITION_DIFF_LAYOUT = {
+        'yaxis': {
+            'autotick': False,
+            'ticks': '',
+            'tick0': 0,
+            'dtick': 20,
+            'ticklen': 200,
+            'tickcolor': '#000'
+        },
+        'shapes' : [
+            # highlight the region associated with top 10 songs
+            {
+                'type': 'rect',
+                'x0': '2017-01-01',
+                'y0': -20,
+                'x1': '2018-01-07',
+                'y1': +20,
+                'fillcolor': 'green',
+                'opacity': 0.2,
+                'line': {
+                    'width': 0
+                }  
+            },
+            {
+                'type': 'rect',
+                'x0': '2017-01-01',
+                'y0': 20,
+                'x1': '2018-01-07',
+                'y1': 200,
+                'fillcolor': 'red',
+                'opacity': 0.2,
+                'line': {
+                    'width': 0
+                }  
+            },
+            {
+                'type': 'rect',
+                'x0': '2017-01-01',
+                'y0': -20,
+                'x1': '2018-01-07',
+                'y1': -200,
+                'fillcolor': 'red',
+                'opacity': 0.2,
+                'line': {
+                    'width': 0
+                }  
+            }
+        ]
+    }
 
-# Other data to extract: jumps above and below current spot. 
-# http://bl.ocks.org/wpoely86/e285b8e4c7b84710e463
-with open ("./us_data.csv") as csvfile:
-    df = pd.read_csv(csvfile)
+    POSITION_HEATMAP_LAYOUT = go.Layout(
+        title='Song Position Heatmap',
+        xaxis = dict(ticks='', nticks=12),
+        yaxis = dict(ticks='' )
+    )
 
-print(df[df['Position'] == 1]['Track Name'].unique())
+    def __init__(self):
+        plotly.offline.init_notebook_mode()
+        with open ("./us_data.csv") as csvfile:
+            self.df = pd.read_csv(csvfile)
+        df = self.df
+        name_and_artists = df[['Track Name','Artist']].drop_duplicates()
+        self.all_songs = list(zip(name_and_artists['Track Name'], name_and_artists['Artist']))
+        self.artists = { name : list(group['Track Name'].drop_duplicates()) for name, group in df.groupby('Artist')}
+
+    def load_data(self):
+        for song in self.all_songs:
+            trackname = song[0]
+            artist = song[1]
+            song_df = df[(df['Track Name'] == trackname)&(df['Artist'] == artist)]
+            position_diffs = song_df['Position'] - song_df.shift()['Position']
+            df.loc[(df['Track Name'] == trackname)&(df['Artist']==artist), 'Position Diff'] = position_diffs
+
+    def songs_at_position(self,position):
+        df = self.df
+        song_df = df[df['Position'] == position]
+        song_df = song_df[['Track Name', 'Artist']].drop_duplicates()
+        return list(zip(song_df['Track Name'],song_df['Artist']))
+
+    def song_scatter_traces(self, attr, songs, fill='tozeroy', mode='linemode'):
+        df = self.df
+        traces = []
+        for song in songs:
+            trackname = song[0]
+            artist = song[1]
+            track = df[(df['Track Name'] == trackname) & (df['Artist'] == artist)]
+            track_x = track['Date']
+            track_y = track[attr]
+            trace = go.Scatter(
+                x = track_x,
+                y = track_y,
+                mode = mode,
+                name = trackname
+            )
+            traces.append(trace)
+        return traces
+
+    def song_box_traces(self, attr, songs):
+        df = self.df
+        traces = []
+        for song in songs:
+            trackname = song[0]
+            artist = song[1]
+            trace = go.Box(
+                x = df[(df['Track Name'] == trackname)&(df['Artist'] == artist)][attr],
+                name = trackname
+            )
+            traces.append(trace)
+        return traces
+
+    def song_heatmap(self, attr, songs):
+        df = self.df
+        z = []
+        date_list = np.array('2017-01-01', dtype=np.datetime64) + np.arange(365)
+        for song in songs:
+            trackname = song[0]
+            artist = song[1]
+            new_row = []
+            song_df = df[(df['Track Name'] == trackname) & (df['Artist'] == artist)]
+            for date in date_list:
+                row = song_df[song_df['Date'] == str(date)]
+                if row.empty:
+                    new_row.append(None)
+                else:
+                    new_row.append(int(row[attr]))
+            z.append(new_row[:])
+
+        result = go.Heatmap(
+                z=z,
+                x=date_list,
+                y=[song[0] for song in songs],
+                colorscale=[ # https://plot.ly/python/colorscales/
+                    [0.0, 'rgb(165,0,38)'], 
+                    [0.1111111111111111, 'rgb(215,48,39)'], 
+                    [0.2222222222222222, 'rgb(244,109,67)'],
+                    [0.3333333333333333, 'rgb(253,174,97)'], 
+                    [0.4444444444444444, 'rgb(254,224,144)'], 
+                    [0.5555555555555556, 'rgb(224,243,248)'],
+                    [0.6666666666666666, 'rgb(171,217,233)'],
+                    [0.7777777777777778, 'rgb(116,173,209)'], 
+                    [0.8888888888888888, 'rgb(69,117,180)'],
+                    [1.0, 'rgb(49,54,149)']
+                ],
+                colorbar = dict(
+                    title = 'Song %s' % attr,
+                    titleside = 'top',
+                    tickvals = [step for step in range(0,211,20)],
+                    ticktext = [str(step) for step in range(0,211,20)],
+                    ticks = 'outside'
+                )
+            )
+        return result
+
+    def create_scatter(self, attr, songs, layout):
+        df = self.df
+        data = self.song_scatter_traces(attr, songs)
+        fig = go.Figure(data=data, layout=layout)
+        plot_div = plotly.offline.plot(fig, output_type='div', include_plotlyjs=False)
+        return plot_div
+
+    def create_heatmap(self,attr,songs,layout):
+        df = self.df
+        data = [self.song_heatmap(attr, songs)]
+        fig = go.Figure(data=data, layout=layout)
+        plot_div = plotly.offline.plot(fig, output_type='div', include_plotlyjs=False)
+        return plot_div
+    
+    def create_boxplots(self, attr, songs):
+        fig = go.Figure(data=self.song_box_traces(attr, songs))
+        plot_div = plotly.offline.plot(fig, output_type='div', include_plotlyjs=False)
+        return plot_div
+    
+
+
+
